c53da578467b71d3ddc661426629d2ad6cb30674
Maximiliano Ezequiel Acuña <thompssonlite@gmail.com>
2024-07-20 22:56:56 -0300

    error de tipeo solucionado

--- src/utils/LazyLoadCard.tsx
+++ src/utils/LazyLoadCard.tsx
@@ -2,11 +2,16 @@ import { DateCovid } from "@/interface/main";
 import dynamic from "next/dynamic";
 import { useInView } from "react-intersection-observer";
 
+interface Props{
+  item: DateCovid,
+  index: number
+}
+
 const DynamicCards = dynamic(() => import("../components/Cards"), {
   loading: () => <div>Loading...</div>,
 });
 
-const LazyLoadCard = ({item,index}) => {
+const LazyLoadCard = ({item,index}:Props) => {
   const {ref, inView} = useInView({
     triggerOnce:true,
     threshold: 0.1
