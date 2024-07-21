9a066f7465dffa223db9d1b1450aaf86bdbe5793
Maximiliano Ezequiel Acuña <thompssonlite@gmail.com>
2024-07-20 23:03:38 -0300

    bug arreglado

--- src/utils/LazyLoadCard.tsx
+++ src/utils/LazyLoadCard.tsx
@@ -2,26 +2,26 @@ import { DateCovid } from "@/interface/main";
 import dynamic from "next/dynamic";
 import { useInView } from "react-intersection-observer";
 
-interface Props{
-  item: DateCovid,
-  index: number
+interface Props {
+  item: DateCovid;
+  index: number;
 }
 
 const DynamicCards = dynamic(() => import("../components/Cards"), {
   loading: () => <div>Loading...</div>,
 });
 
-const LazyLoadCard = ({item,index}:Props) => {
-  const {ref, inView} = useInView({
-    triggerOnce:true,
-    threshold: 0.1
+const LazyLoadCard: React.FC<Props> = ({ item, index }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
   });
 
-  return <>
+  return (
     <div ref={ref}>
       {inView ? <DynamicCards item={item} index={index} /> : <p>Loading...</p>}
     </div>
-  </>;
+  );
 };
 
 export default LazyLoadCard;
